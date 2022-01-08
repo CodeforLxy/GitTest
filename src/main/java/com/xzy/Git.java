bbf73b2a696f0de2fba7f9341cf6e117f18e75e2
CodeforLxy <82518150+CodeforLxy@users.noreply.github.com>
2022-01-08 17:05:53 +0800

    pull commit

--- src/main/java/com/xzy/Git.java
+++ src/main/java/com/xzy/Git.java
@@ -8,5 +8,6 @@ public class Git {
         System.out.println("master commit1");
         System.out.println("hot-fix commit2");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
