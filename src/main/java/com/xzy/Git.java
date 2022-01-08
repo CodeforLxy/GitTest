32cac983ed9a61eef11da3bf6abdd5c26361b513
codeforlxy <569487022@qq.com>
2022-01-08 16:46:47 +0800

    master commit1

--- src/main/java/com/xzy/Git.java
+++ src/main/java/com/xzy/Git.java
@@ -5,5 +5,6 @@ public class Git {
         System.out.println("hello git!");
         System.out.println("hello git2");
         System.out.println("hot-fix commit1");
+        System.out.println("master commit1");
     }
 }
